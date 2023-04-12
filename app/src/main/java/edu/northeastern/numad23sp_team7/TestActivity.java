7ace1b42087a3ea9eaade389b6e696c844aa8283
Qiansha Meng <meng.q@northeastern.edu>
2023-04-11 21:04:33 -0700

    updata TestActivity

--- app/src/main/java/edu/northeastern/numad23sp_team7/TestActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team7/TestActivity.java
@@ -2,11 +2,18 @@ package edu.northeastern.numad23sp_team7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -16,10 +23,27 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-
         imageView = findViewById(R.id.imageViewTestForImageDisplay);
-
         String imageUrl = "content://com.google.android.apps.photos.contentprovider/-1/1/content%3A%2F%2Fmedia%2Fexternal%2Fimages%2Fmedia%2F15/ORIGINAL/NONE/image%2Fjpeg/57271938";
+
+        // Not working
         Picasso.get().load(Uri.parse(imageUrl)).into(imageView);
+
+        // Not working
+//        try {
+//            imageView.setImageBitmap(downloadImage(imageUrl));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+
+    public Bitmap downloadImage(String imageUrl) throws IOException {
+        URL url = new URL(imageUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoInput(true);
+        connection.connect();
+        InputStream input = connection.getInputStream();
+        Bitmap bitmap = BitmapFactory.decodeStream(input);
+        return bitmap;
     }
 }
