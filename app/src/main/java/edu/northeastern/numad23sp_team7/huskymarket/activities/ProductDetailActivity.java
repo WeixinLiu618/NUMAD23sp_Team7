1f2bb5dce8317053ae263c1ee7a14027e664b18a
zili zhou <zilizzhou@gmail.com>
2023-04-14 22:59:13 -0700

    product detail updates

--- app/src/main/java/edu/northeastern/numad23sp_team7/huskymarket/activities/ProductDetailActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team7/huskymarket/activities/ProductDetailActivity.java
@@ -4,10 +4,12 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -16,9 +18,11 @@ import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import edu.northeastern.numad23sp_team7.R;
 import edu.northeastern.numad23sp_team7.databinding.ActivityProductDetailBinding;
+import edu.northeastern.numad23sp_team7.huskymarket.adapter.SearchResultAdapter;
 import edu.northeastern.numad23sp_team7.huskymarket.database.UserDao;
 import edu.northeastern.numad23sp_team7.huskymarket.model.ChatMessage;
 import edu.northeastern.numad23sp_team7.huskymarket.model.Product;
@@ -30,6 +34,10 @@ public class ProductDetailActivity extends AppCompatActivity {
     private FirebaseFirestore database;
     private String productId;
     private Product product;
+    private User loggedInUser;
+    private AtomicInteger saved;
+    private UserDao userDao;
+    private int bookmarkIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +50,7 @@ public class ProductDetailActivity extends AppCompatActivity {
         Intent intentFromSearch = getIntent();
         productId = intentFromSearch.getStringExtra(Constants.KEY_PRODUCT_ID);
 
+
         //load product detail from database
         database.collection(Constants.KEY_COLLECTION_PRODUCTS).document(productId).get()
                 .addOnSuccessListener(documentSnapshot -> {
@@ -52,11 +61,38 @@ public class ProductDetailActivity extends AppCompatActivity {
                         binding.thePriceOfProduct.setText(String.valueOf(product.getPrice()));
                         binding.location.setText(product.getLocation());
                         //TODO: get Image
+                        //bookmark
+                        //Drawable bookmarkIcon;
+                        productId = product.getProductId();
+                        saved = new AtomicInteger(isFavorite(productId));
+                        if (saved.get() == 1 ) {
+                            bookmarkIcon = R.drawable.ic_bookmarked;
+                        } else {
+                            bookmarkIcon = R.drawable.ic_bookmarked_not;
+                        }
+                        binding.bookmarkProductDetail.setBackground(
+                                ContextCompat.getDrawable(binding.bookmarkProductDetail.getContext(), bookmarkIcon ));
                     }
+
                 });
+        binding.bookmarkProductDetail.setOnClickListener(view -> {
+            if (saved.get() == 1) {
+                saved.set(0);
+                loggedInUser.getFavorites().remove(productId);
+                userDao.removeItemFromFavorites(loggedInUser.getId(), productId);
+                bookmarkIcon = R.drawable.ic_bookmarked_not;
+            } else if (saved.get() == 0) {
+                saved.set(1);
+                loggedInUser.getFavorites().add(productId);
+                userDao.addItemToFavorites(loggedInUser.getId(), productId);
+                bookmarkIcon = R.drawable.ic_bookmarked;
+            }
+            binding.bookmarkProductDetail.setBackground(
+                    ContextCompat.getDrawable(binding.bookmarkProductDetail.getContext(),bookmarkIcon));
+        });
+
         binding.layoutSendMessage.setOnClickListener(v -> {
             //start chat with seller
-            // TODO: go to ChatActivity, passing post user (not post user id)
             UserDao userDao = new UserDao();
             userDao.getUserById(product.getPostUserId(), receiver -> {
                 Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
@@ -66,5 +102,20 @@ public class ProductDetailActivity extends AppCompatActivity {
         });
         // back button
         binding.backBtn.setOnClickListener(v -> onBackPressed());
+    }
+    private int isFavorite(String productId) {
+        // Saved status
+        // 0: not saved
+        // 1: saved
+        // 2: not looged-in™
+        if (loggedInUser == null) {
+            return 2;
+        }
+
+        if (loggedInUser.getFavorites().contains(productId)) {
+            return 1;
+        }
+
+        return 0;
     }
 }
