6d85bc27cee98eaef30ec0646cdf17894ccbc06e
WeixinLiu618 <liu.weix@northeastern.edu>
2023-04-16 17:27:04 -0700

    fix my sellings fragment and modify selling item layout

--- app/src/main/java/edu/northeastern/numad23sp_team7/huskymarket/activities/SellingsFragment.java
+++ app/src/main/java/edu/northeastern/numad23sp_team7/huskymarket/activities/SellingsFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -43,32 +44,19 @@ public class SellingsFragment extends Fragment implements MySellingsCardClickLis
 
         database = FirebaseFirestore.getInstance();
         preferenceManager = new PreferenceManager(requireContext());
-
-        // Inflate the layout for this fragment
         binding = FragmentSellingsBinding.inflate(getLayoutInflater());
-        mySellings = new ArrayList<>();
-        mySellingsAdapter = new MySellingsAdapter(mySellings, getContext(), this);
-        binding.recyclerViewSellings.setAdapter(mySellingsAdapter);
-
         String userId = preferenceManager.getString(Constants.KEY_USER_ID);
 
 
         // TODO
-        productDao.getMyPostsProductsForUser(userId, mySellings -> {
+        productDao.getMyPostsProductsForUser(userId, products -> {
+            this.mySellings = products;
+            mySellingsAdapter = new MySellingsAdapter(mySellings, this);
+            binding.recyclerViewSellings.setAdapter(mySellingsAdapter);
             mySellingsAdapter.notifyDataSetChanged();
-            Log.d(TAG, "onCreateView: "+mySellings.size());
-            if (mySellings.size() == 0) {
-                binding.noPostsPrompt.setVisibility(View.VISIBLE);
-                binding.recyclerViewSellings.setVisibility(View.GONE);
-            } else {
-                binding.recyclerViewSellings.smoothScrollToPosition(0);
-                binding.recyclerViewSellings.setVisibility(View.VISIBLE);
-                binding.progressBar.setVisibility(View.GONE);
-            }
-            binding.progressBar.setVisibility(View.GONE);
+            Log.d(TAG, "onCreateView: " + mySellings.size());
+            loading(false);
         });
-
-
 
 
         return binding.getRoot();
@@ -78,6 +66,26 @@ public class SellingsFragment extends Fragment implements MySellingsCardClickLis
 
     @Override
     public void onEditClick(int position) {
+
+    }
+
+    private void loading(boolean isLoading) {
+        if (isLoading) {
+            binding.progressBar.setVisibility(View.VISIBLE);
+            binding.recyclerViewSellings.setVisibility(View.GONE);
+            binding.noPostsPrompt.setVisibility(View.GONE);
+        } else {
+            binding.progressBar.setVisibility(View.GONE);
+            if (mySellings.size() == 0) {
+                binding.noPostsPrompt.setVisibility(View.VISIBLE);
+                binding.recyclerViewSellings.setVisibility(View.GONE);
+            } else {
+                binding.recyclerViewSellings.smoothScrollToPosition(0);
+                Log.d(TAG, "loading: " + "来到这里");
+                binding.recyclerViewSellings.setVisibility(View.VISIBLE);
+                binding.noPostsPrompt.setVisibility(View.GONE);
+            }
+        }
 
     }
 }
